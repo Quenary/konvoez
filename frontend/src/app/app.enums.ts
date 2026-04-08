68fdd572d569bbac2de44bacd815e01d5b57850d
Quenary <evgeny.savin.nord@gmail.com>
2026-04-09 02:12:40 +0300

    feat: preserve mute state

--- frontend/src/app/app.enums.ts
+++ frontend/src/app/app.enums.ts
@@ -2,4 +2,6 @@ export enum EStorageKey {
   AUDIO_INPUT = 'konvoez-audio-input',
   AUDIO_OUTPUT = 'konvoez-audio-output',
   PEER_GAIN_LEVELS = 'konvoez-peer-gain-levels',
+  MICROPHONE_MUTED = 'konvoez-microphone-muted',
+  SPEAKER_MUTED = 'konvoez-speaker-muted',
 }
