10ac85cb5645b5a027516f9243202f10dfd83f18
Quenary <evgeny.savin.nord@gmail.com>
2026-07-24 04:28:44 +0300

    refactor(taiga): settings

--- apps/frontend/src/app/features/settings/settings.component.ts
+++ apps/frontend/src/app/features/settings/settings.component.ts
@@ -4,46 +4,73 @@ import {
   computed,
   inject,
   resource,
+  signal,
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import {
-  from,
-  switchMap,
-  catchError,
-  of,
-  lastValueFrom,
-  firstValueFrom,
-} from 'rxjs';
+import { from, switchMap, catchError, of, lastValueFrom } from 'rxjs';
 import { MediaDevicesService } from '@core/services/media-devices.service';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
-import { MessageService } from 'primeng/api';
 import { Store } from '@ngrx/store';
 import { selectAudioInput, selectAudioOutput } from './settings.selectors';
-import { SelectModule } from 'primeng/select';
 import { SettingsActions } from './settings.actions';
 import { FormsModule } from '@angular/forms';
-import { IftaLabelModule } from 'primeng/iftalabel';
-import { TooltipModule } from 'primeng/tooltip';
 import { AuthActions } from '../auth/auth.actions';
-import { ButtonModule } from 'primeng/button';
-import { FileUploadHandlerEvent, FileUploadModule } from 'primeng/fileupload';
-import { AvatarModule } from 'primeng/avatar';
 import { AvatarsApiService } from '../avatars/avatars-api.service';
-import { maxAvatarSize } from '@konvoez/shared';
 import { selectMe } from '../auth/auth.selectors';
-import { SettingsApiService } from './settings-api.service';
+import {
+  TuiButton,
+  TuiDataList,
+  TuiDropdown,
+  TuiError,
+  TuiIcon,
+  TuiInput,
+  tuiItemsHandlersProvider,
+  TuiLabel,
+  TuiNotification,
+  TuiNotificationService,
+} from '@taiga-ui/core';
+import {
+  TuiAvatar,
+  TuiSelect,
+  TuiDataListWrapper,
+  TuiChevron,
+  TuiInitialsPipe,
+  TuiTooltip,
+} from '@taiga-ui/kit';
+import { TuiCardLarge, TuiForm, TuiHeader } from '@taiga-ui/layout';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-settings',
   imports: [
-    SelectModule,
     FormsModule,
-    IftaLabelModule,
     TranslatePipe,
-    TooltipModule,
-    ButtonModule,
-    FileUploadModule,
-    AvatarModule,
+    TuiButton,
+    TuiCardLarge,
+    TuiError,
+    TuiForm,
+    TuiHeader,
+    TuiIcon,
+    TuiInput,
+    TuiNotification,
+    TuiAvatar,
+    TuiSelect,
+    TuiLabel,
+    TuiDataListWrapper,
+    TuiDataList,
+    TuiDropdown,
+    TuiChevron,
+    TuiInitialsPipe,
+    NgTemplateOutlet,
+    TuiTooltip,
+  ],
+  providers: [
+    tuiItemsHandlersProvider({
+      stringify: signal((a: MediaDeviceInfo) => a.label),
+      identityMatcher: signal(
+        (a: MediaDeviceInfo, b: MediaDeviceInfo) => a.deviceId === b.deviceId,
+      ),
+    }),
   ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
@@ -52,16 +79,21 @@ import { SettingsApiService } from './settings-api.service';
 export class SettingsComponent {
   private readonly store = inject(Store);
   private readonly mediaDevicesService = inject(MediaDevicesService);
-  private readonly messageService = inject(MessageService);
+  private readonly tuiNotificationsService = inject(TuiNotificationService);
   private readonly translateService = inject(TranslateService);
   private readonly avatarsApiService = inject(AvatarsApiService);
-  private readonly settingsApiService = inject(SettingsApiService);
 
-  protected readonly settings = resource({
-    loader: () => firstValueFrom(this.settingsApiService.list()),
-  });
-
+  /**
+   * Current user info
+   */
   protected readonly me = this.store.selectSignal(selectMe);
+  /**
+   * Current user name
+   */
+  protected readonly username = computed(() => this.me()?.username ?? '');
+  /**
+   * Current avatar signed url
+   */
   protected readonly avatarUrl = resource({
     params: () => ({ url: this.me()?.avatar }),
     loader: (params) =>
@@ -69,47 +101,82 @@ export class SettingsComponent {
         ? lastValueFrom(this.avatarsApiService.getUrl(params.params.url))
         : Promise.resolve(null),
   });
-
-  protected readonly maxAvatarSize = maxAvatarSize;
+  /**
+   * Selected audio input
+   */
   protected readonly audioInput = this.store.selectSignal(selectAudioInput);
+  /**
+   * Is selected audio input available
+   */
+  protected readonly audioInputAvailable = computed(() => {
+    const audioInput = this.audioInput();
+    const audioInputList = this.audioInputList();
+    return (
+      audioInput &&
+      audioInputList.some((item) => item.deviceId === audioInput.deviceId)
+    );
+  });
+  /**
+   * Selected audio output
+   */
   protected readonly audioOutput = this.store.selectSignal(selectAudioOutput);
-
-  private readonly devices = toSignal(
-    from(this.mediaDevicesService.getUserMedia({ audio: true })).pipe(
-      switchMap(() => from(this.mediaDevicesService.enumerateDevices())),
-      catchError(() => {
-        this.messageService.add({
-          severity: 'error',
-          summary: this.translateService.instant(
-            'CARDS.CARD.SCAN.PERMISSION_ERROR',
-          ),
-        });
-        return of([]);
-      }),
-    ),
-    { initialValue: [] },
-  );
+  /**
+   * Is selected audio output available
+   */
+  protected readonly audioOutputAvailable = computed(() => {
+    const audioOutput = this.audioOutput();
+    const audioOutputList = this.audioOutputList();
+    return (
+      audioOutput &&
+      audioOutputList.some((item) => item.deviceId === audioOutput.deviceId)
+    );
+  });
+  /**
+   * List of available inputs
+   */
   protected readonly audioInputList = computed(() => {
     const devices = this.devices() ?? [];
     return devices.filter((d) => d.kind == 'audioinput');
   });
+  /**
+   * List of available outputs
+   */
   protected readonly audioOutputList = computed(() => {
     const devices = this.devices() ?? [];
     return devices.filter((d) => d.kind == 'audiooutput');
   });
 
-  private getDeviceById(deviceId: string): MediaDeviceInfo {
-    const devices = this.devices() ?? [];
-    return devices.find((d) => d.deviceId == deviceId) as MediaDeviceInfo;
-  }
+  /**
+   * All audio devices
+   */
+  private readonly devices = toSignal(
+    from(this.mediaDevicesService.getUserMedia({ audio: true })).pipe(
+      switchMap(() => from(this.mediaDevicesService.enumerateDevices())),
+      catchError(() => {
+        this.tuiNotificationsService
+          .open(this.translateService.instant('SETTINGS.PERMISSION_ERROR'), {
+            appearance: 'negative',
+            autoClose: 5000,
+            closable: true,
+          })
+          .subscribe();
+        return of([]);
+      }),
+    ),
+    { initialValue: [] },
+  );
 
-  protected onSelectAudioInput(deviceId: string) {
-    const audioInput = this.getDeviceById(deviceId);
+  /**
+   * Select audio input
+   */
+  protected onSelectAudioInput(audioInput: MediaDeviceInfo) {
     this.store.dispatch(SettingsActions.setAudioInput({ audioInput }));
   }
 
-  protected onSelectAudioOutput(deviceId: string) {
-    const audioOutput = this.getDeviceById(deviceId);
+  /**
+   * Select audio output
+   */
+  protected onSelectAudioOutput(audioOutput: MediaDeviceInfo) {
     this.store.dispatch(SettingsActions.setAudioOutput({ audioOutput }));
   }
 
@@ -117,11 +184,16 @@ export class SettingsComponent {
     this.store.dispatch(AuthActions.requestLogout());
   }
 
-  onAvatarUpload($event: FileUploadHandlerEvent) {
-    this.store.dispatch(
-      AuthActions.uploadAvatar({
-        file: $event.files[0],
-      }),
-    );
+  protected uploadAvatar(input: HTMLInputElement): void {
+    const file = input.files ? Array.from(input.files)[0] : null;
+    input.files = null;
+    input.blur();
+    if (file) {
+      this.store.dispatch(
+        AuthActions.uploadAvatar({
+          file,
+        }),
+      );
+    }
   }
 }
