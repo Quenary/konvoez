3dcbfc0dc060fc8b2fd4e7999d9b5bf17ed6f1dc
Quenary <evgeny.savin.nord@gmail.com>
2026-07-30 15:17:24 +0300

    fix(backend): run migrations on startup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { MikroORM } from '@mikro-orm/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const orm = app.get(MikroORM);
+  await orm.migrator.up();
+
   app.setGlobalPrefix('/api');
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
