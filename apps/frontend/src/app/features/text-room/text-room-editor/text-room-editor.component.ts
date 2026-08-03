ddbe2144d2fd3e28bab314dc6e801455eb782228
Eugene Savin <evgeny.savin.nord@gmail.com>
2026-08-04 02:43:09 +0300

    feat(frontend): send on enter

--- apps/frontend/src/app/features/text-room/text-room-editor/text-room-editor.component.ts
+++ apps/frontend/src/app/features/text-room/text-room-editor/text-room-editor.component.ts
@@ -16,6 +16,7 @@ import {
   type TuiEditorToolType,
 } from '@taiga-ui/editor';
 import { v4 } from 'uuid';
+import { createKeyBindingExtension } from '../../../core/tiptap/create-key-binding-extension';
 import { TextRoomStore } from '../text-room.store';
 
 const EMPTY_HTML_PATTERN = /^(\s*<p>(\s|<br\s*\/?>)*<\/p>\s*)*$/i;
@@ -34,36 +35,51 @@ const EMPTY_HTML_PATTERN = /^(\s*<p>(\s|<br\s*\/?>)*<\/p>\s*)*$/i;
       provide: TUI_EDITOR_SANITIZER,
       useClass: NgDompurifySanitizer,
     },
-    provideTuiEditor({
-      heading: false,
-      bulletList: false,
-      orderedList: false,
-      listItem: false,
-      taskList: false,
-      taskItem: false,
-      horizontalRule: false,
-      textAlign: false,
-      subscript: false,
-      superscript: false,
-      fontColor: false,
-      backgroundColor: false,
-      fontSize: false,
-      jumpAnchor: false,
-      fileLink: false,
-      table: false,
-      tableCell: false,
-      tableRow: false,
-      tableHeader: false,
-      tableCellBackground: false,
-      details: false,
-      detailsSummary: false,
-      detailsContent: false,
-      image: false,
-      video: false,
-      audio: false,
-      source: false,
-      iframe: false,
-    }),
+    provideTuiEditor(
+      {
+        heading: false,
+        bulletList: false,
+        orderedList: false,
+        listItem: false,
+        taskList: false,
+        taskItem: false,
+        horizontalRule: false,
+        textAlign: false,
+        subscript: false,
+        superscript: false,
+        fontColor: true,
+        backgroundColor: false,
+        fontSize: false,
+        jumpAnchor: false,
+        fileLink: false,
+        table: false,
+        tableCell: false,
+        tableRow: false,
+        tableHeader: false,
+        tableCellBackground: false,
+        details: false,
+        detailsSummary: false,
+        detailsContent: false,
+        image: false,
+        video: false,
+        audio: false,
+        source: false,
+        iframe: false,
+        enter: false,
+      },
+      (injector) => {
+        const component = injector.get(TextRoomEditorComponent);
+
+        return Promise.resolve(
+          createKeyBindingExtension('Enter', ({ editor }) => {
+            if (editor.isActive('codeBlock')) return false;
+
+            component.onSubmit();
+            return true;
+          }),
+        );
+      },
+    ),
   ],
   templateUrl: './text-room-editor.component.html',
   styleUrl: './text-room-editor.component.scss',
@@ -82,6 +98,7 @@ export class TextRoomEditorComponent {
     TuiEditorTool.Italic,
     TuiEditorTool.Underline,
     TuiEditorTool.Strikethrough,
+    TuiEditorTool.Color,
     TuiEditorTool.Quote,
     TuiEditorTool.Code,
     TuiEditorTool.Link,
